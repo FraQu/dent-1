6d84e37e1268b946b74edea01e16bf49a69e948a
Adrian Kasprzyk <xa.kasprzyk@gmail.com>
2020-09-06 12:55:37 +0200

    Rework of admin, user, user profile, login and register

--- website/admin.py
+++ website/admin.py
@@ -1,7 +1,47 @@
 from django.contrib import admin
-from .models import Customer
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth.models import Group
+from django.utils.translation import ugettext_lazy as _
+
+from .models import User, UserProfile
 
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    """Define admin model for custom User model with no username field."""
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('full_name',)}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email',),
+        }),
+    )
+    list_display = ('email', 'full_name', 'is_superuser', 'is_staff')
+    search_fields = ('email', 'full_name')
+    ordering = ('email',)
 
-admin.site.register(Customer)
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ('email',)}),
+        (_('Personal info'), {'fields': ('full_name', 'phone', 'age', 'gender')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'full_name', 'phone', 'age', 'gender')
+    search_fields = ('email', 'full_name')
+    ordering = ('email',)
+
+
+admin.site.unregister(Group)
--- website/forms.py
+++ website/forms.py
@@ -1,17 +1,92 @@
-from django.forms import ModelForm
-from django.contrib.auth.forms import UserCreationForm
-from .models import *
+from django import forms
+from django.contrib.auth import get_user_model
+
+from .models import UserProfile, User
 
 
-class CustomerForm(ModelForm):
+class UserAdminCreationForm(forms.ModelForm):
+
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+
     class Meta:
-        model = Customer
+        model = User
+        fields = ('email',)
+
+    def clean_password(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 is None:
+            raise forms.ValidationError("Fill first password field.")
+        if password2 is None:
+            raise forms.ValidationError("Fill second password field.")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match.")
+        return password2
+
+    def save(self, commit=True):
+        user = super(UserAdminCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(label='Email')
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+
+class RegisterForm(forms.ModelForm):
+    """Register Form based on email and password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email',)
+
+    def clean_password2(self):
+        """Check passwords fields."""
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if not password1:
+            raise forms.ValidationError("Fill first password field.")
+        if password2 is None:
+            raise forms.ValidationError("Fill second password field.")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match.")
+        return password2
+
+    def clean_email(self):
+        """Check email field."""
+        email = self.cleaned_data.get('email')
+        email_ex = User.objects.filter(email=email)
+        if not email:
+            raise forms.ValidationError("Fill email field.")
+        if email_ex.exists():
+            raise forms.ValidationError(f"{email} - this email is already taken.")
+        if not email:
+            raise forms.ValidationError("Fill email field.")
+        return email
+
+    def save(self, commit=True):
+        """Save email and password as User."""
+        user = super(RegisterForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        user.active = True
+        if commit:
+            user.save()
+            UserProfile.objects.create(email=user)
+        return user
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
         fields = '__all__'
         exclude = ['user']
 
 
-class CreateUserForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+User = get_user_model()
 
