1c434c35ff80edc112713335c3f93b286e2a0f3d
bigos1234 <martynakalkowska9@gmail.com>
2020-12-11 13:18:06 +0100

    script.js removed

--- static/website/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.nav-button').click(function() {
-    $('.nav-button').toggleClass('change');
-    });
-});
-
-function load_add_customer() {
-     document.getElementById("body_content").innerHTML='<object type="text/html" data="templates/website/add_customer.html" ></object>';
-}
